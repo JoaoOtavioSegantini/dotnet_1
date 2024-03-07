af575c3a54fa0f1395c2c8d7145457856c4b2c5d
João Otávio <joaotavioos@outlook.com>
2024-03-07 07:21:06 -0300

    feat: implements IStockRepository interface ✨

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -1,4 +1,5 @@
 using api.Data;
+using api.Dtos.Stock;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -15,9 +16,53 @@ namespace api.Repository
       _context = dBContext;
     }
 
+    public async Task<Stock> CreateAsync(Stock stockModel)
+    {
+      await _context.Stocks.AddAsync(stockModel);
+      await _context.SaveChangesAsync();
+      return stockModel;
+    }
+
+    public async Task<Stock?> DeleteAsync(int id)
+    {
+      var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+      if (stockModel is null)
+      {
+        return null;
+      }
+
+      _context.Stocks.Remove(stockModel);
+      await _context.SaveChangesAsync();
+      return stockModel;
+    }
+
     public Task<List<Stock>> GetAllAsync()
     {
       return _context.Stocks.ToListAsync();
+    }
+
+    public async Task<Stock?> GetByIdAsync(int id)
+    {
+      return await _context.Stocks.FindAsync(id);
+    }
+
+    public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
+    {
+      var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+      if (stockModel is null) {
+        return null;
+      }
+      stockModel.Symbol = stockDto.Symbol;
+      stockModel.LastDiv = stockDto.LastDiv;
+      stockModel.Industry = stockDto.Industry;
+      stockModel.Purchase = stockDto.Purchase;
+      stockModel.MarketCap = stockDto.MarketCap;
+      stockModel.CompanyName = stockDto.CompanyName;
+
+      await _context.SaveChangesAsync();
+
+      return stockModel;
+
     }
   }
 }
