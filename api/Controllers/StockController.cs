c6ba3f832eee415034ab276f41cc68b058926e8d
João Otávio <joaotavioos@outlook.com>
2024-03-07 04:39:33 -0300

    refactor: add refactor to Repository Pattern 📦
    add refactor in StocksController and changes GetAll method to repository pattern

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using api.Data;
 using api.Mappers;
 using api.Dtos.Stock;
 using Microsoft.EntityFrameworkCore;
+using api.Interfaces;
 
 namespace api.Controllers
 {
@@ -11,15 +12,17 @@ namespace api.Controllers
   public class StockController : ControllerBase
   {
     private readonly ApplicationDBContext _context;
-    public StockController(ApplicationDBContext context)
+    private readonly IStockRepository _stockRepo;
+    public StockController(ApplicationDBContext context, IStockRepository stockRepo)
     {
       _context = context;
+      _stockRepo = stockRepo;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-      var stocks = await _context.Stocks.ToListAsync();
+      var stocks = await _stockRepo.GetAllAsync();
       var stockDto = stocks.Select(s => s.ToStockDto());
       return Ok(stocks);
 
