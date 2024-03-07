6bb5d0bd57458b8fe1c64f39f44e104475c1be4f
João Otávio <joaotavioos@outlook.com>
2024-03-07 10:22:57 -0300

    feat: implements GetByIdAsync in comment repository ✨

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -16,5 +16,12 @@ namespace api.Repository
     {
       return await _context.Comments.ToListAsync();
     }
+
+    public async Task<Comment?> GetByIdAsync(int id)
+    {
+      
+      return await _context.Comments.FindAsync(id);
+
+    }
   }
 }
