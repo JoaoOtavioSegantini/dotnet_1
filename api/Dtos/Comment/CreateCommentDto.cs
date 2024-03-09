2b7ea8f7b51c807eafec75bde45b5aca51ba5e64
João Otávio <joaotavioos@outlook.com>
2024-03-09 19:06:54 -0300

    feat: add data validation in create comment dto ✨

--- api/Dtos/Comment/CreateCommentDto.cs
+++ api/Dtos/Comment/CreateCommentDto.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace api.Dtos.Comment
 {
   public class CreateCommentDto
   {
 
+    [Required]
+    [MinLength(5, ErrorMessage = "Title must be have 5 characters")]
+    [MaxLength(280, ErrorMessage = "Title cannot be over 280 characters")]
     public String Title { get; set; } = string.Empty;
 
+    [Required]
+    [MinLength(5, ErrorMessage = "Content must be have 5 characters")]
+    [MaxLength(280, ErrorMessage = "Content cannot be over 280 characters")]
     public String Content { get; set; } = string.Empty;
 
   }
