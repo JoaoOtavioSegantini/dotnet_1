6dc9b063b1979da9727e84c99f9d34eaa254cf10
João Otávio <joaotavioos@outlook.com>
2024-03-09 19:09:00 -0300

    feat: add data validation in stock controller ✨

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -21,6 +21,12 @@ namespace api.Controllers
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
+
+      if (!ModelState.IsValid)
+      {
+          return BadRequest(ModelState);
+      }
+
       var stocks = await _stockRepo.GetAllAsync();
       var stockDto = stocks.Select(s => s.ToStockDto());
       return Ok(stocks);
@@ -30,6 +36,12 @@ namespace api.Controllers
     [HttpGet("{id:int}")]
     public async Task<IActionResult> GetById([FromRoute] int id)
     {
+
+      if (!ModelState.IsValid)
+      {
+          return BadRequest(ModelState);
+      }
+
       var stock = await _stockRepo.GetByIdAsync(id);
 
       if (stock == null)
@@ -45,6 +57,12 @@ namespace api.Controllers
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
     {
+
+      if (!ModelState.IsValid)
+      {
+          return BadRequest(ModelState);
+      }
+
       if (stockDto is null)
       {
         throw new ArgumentNullException(nameof(stockDto));
@@ -59,6 +77,11 @@ namespace api.Controllers
     [Route("{id:int}")]
     public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto stockDto)
     {
+
+      if (!ModelState.IsValid)
+      {
+          return BadRequest(ModelState);
+      }
 
       var stockModel = await _stockRepo.UpdateAsync(id, stockDto);
 
@@ -77,6 +100,12 @@ namespace api.Controllers
     [Route("{id:int}")]
     public async Task<IActionResult> Delete([FromRoute] int id)
     {
+
+      if (!ModelState.IsValid)
+      {
+          return BadRequest(ModelState);
+      }
+
       var stockModel = await _stockRepo.DeleteAsync(id);
 
       if (stockModel is null)
