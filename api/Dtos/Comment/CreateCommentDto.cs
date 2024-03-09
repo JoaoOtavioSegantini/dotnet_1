29ea4bd48728109ea3993a0855fc6b0202edc07e
João Otávio <joaotavioos@outlook.com>
2024-03-09 09:46:49 -0300

    feat: add create comment dto

---
+++ api/Dtos/Comment/CreateCommentDto.cs
@@ -0,0 +1,11 @@
+namespace api.Dtos.Comment
+{
+  public class CreateCommentDto
+  {
+
+    public String Title { get; set; } = string.Empty;
+
+    public String Content { get; set; } = string.Empty;
+
+  }
+}
