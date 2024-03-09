cf19c24cc54b06c699b8f6c51f3170946da94801
João Otávio <joaotavioos@outlook.com>
2024-03-09 10:03:45 -0300

    chore: add identation ♻️

--- api/Mappers/CommentMapper.cs
+++ api/Mappers/CommentMapper.cs
@@ -5,25 +5,26 @@ namespace api.Mappers
 {
   public static class CommentMapper
   {
-      public static CommentDto ToCommentDto(this Comment commentModel)
+    public static CommentDto ToCommentDto(this Comment commentModel)
+    {
+      return new CommentDto
       {
-         return new CommentDto
-         {
-           Id = commentModel.Id,
-           Title = commentModel.Title,
-           Content = commentModel.Content,
-           CreatedOn = commentModel.CreatedOn,
-           StockId = commentModel.StockId,
-         };
-      }
-      public static Comment ToCommentFromCreate(this CreateCommentDto commentDto, int stockId)
+        Id = commentModel.Id,
+        Title = commentModel.Title,
+        Content = commentModel.Content,
+        CreatedOn = commentModel.CreatedOn,
+        StockId = commentModel.StockId,
+      };
+    }
+
+    public static Comment ToCommentFromCreate(this CreateCommentDto commentDto, int stockId)
+    {
+      return new Comment
       {
-         return new Comment
-         {
-           Title = commentDto.Title,
-           Content = commentDto.Content,
-           StockId = stockId,
-         };
-      }
+        Title = commentDto.Title,
+        Content = commentDto.Content,
+        StockId = stockId,
+      };
+    }
   }
 }
