d69385f669189e6de0b679017328c71c6adb1b39
João Otávio <joaotavioos@outlook.com>
2024-03-10 09:28:18 -0300

    feat: implements ordenation by symbol in stock controller ✨

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -43,12 +43,20 @@ namespace api.Repository
 
       if (!string.IsNullOrWhiteSpace(query.CompanyName))
       {
-          stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
+        stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
       }
 
       if (!string.IsNullOrWhiteSpace(query.Symbol))
       {
-          stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
+        stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
+      }
+
+      if (!string.IsNullOrWhiteSpace(query.SortBy))
+      {
+        if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+        {
+          stocks = query.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+        }
       }
 
       return await stocks.ToListAsync();
