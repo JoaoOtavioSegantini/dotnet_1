c752ef037e3cdac466c6b477ebca682f746618c4
João Otávio <joaotavioos@outlook.com>
2024-03-10 17:13:16 -0300

    feat: add authentication and authorization config ✨

--- api/Program.cs
+++ api/Program.cs
@@ -1,7 +1,11 @@
 using api.Data;
 using api.Interfaces;
+using api.Models;
 using api.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,15 +13,49 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
     {
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+      options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
     });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     {
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection"));
+      options.UseSqlServer(
+          builder.Configuration.GetConnectionString("DefaultConnection"));
     });
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+    {
+      options.Password.RequireDigit = true;
+      options.Password.RequireLowercase = true;
+      options.Password.RequireUppercase = true;
+      options.Password.RequireNonAlphanumeric = true;
+      options.Password.RequiredLength = 12;
+    }).AddEntityFrameworkStores<ApplicationDBContext>();
+
+builder.Services.AddAuthentication(options =>
+    {
+      options.DefaultAuthenticateScheme =
+      options.DefaultChallengeScheme =
+      options.DefaultForbidScheme =
+      options.DefaultScheme =
+      options.DefaultSignInScheme =
+      options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+      {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuer = true,
+          ValidIssuer = builder.Configuration["JWT:Issuer"],
+          ValidateAudience = true,
+          ValidAudience = builder.Configuration["JWT:Audience"],
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:Signingkey"])
+            )
+        };
+      });
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
@@ -32,6 +70,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 var summaries = new[]
@@ -41,15 +83,15 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
     {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-         DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-         Random.Shared.Next(-20, 55),
-         summaries[Random.Shared.Next(summaries.Length)]
-        ))
-    .ToArray();
-    return forecast;
+      var forecast = Enumerable.Range(1, 5).Select(index =>
+          new WeatherForecast
+          (
+           DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+          ))
+      .ToArray();
+      return forecast;
     })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
