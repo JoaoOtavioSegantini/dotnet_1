b4f05edb4d431ff76cdc22f0f1671b3cd4fc35ef
João Otávio <joaotavioos@outlook.com>
2024-03-10 10:40:25 -0300

    feat: add pagination params ✨

--- api/Helpers/QueryObject.cs
+++ api/Helpers/QueryObject.cs
@@ -11,6 +11,10 @@ namespace api.Helpers
 
     public bool IsDescending { get; set; } = false;
 
+    public int PageNumber { get; set; } = 1;
+
+    public int PageSize { get; set; } = 20;
+
   }
 
 }
