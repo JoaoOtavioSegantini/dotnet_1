86a3ad148eb9e1ad3be6c7dcd7ced305877072d0
João Otávio <joaotavioos@outlook.com>
2024-03-10 20:38:48 -0300

    feat: add roles migrations ✨

---
+++ api/Migrations/20240310230129_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "011a4966-8ec4-401e-8ce2-6b852ab21816", null, "Admin", "ADMIN" },
+                    { "7156fad9-89be-4b18-b413-aa374030e2b7", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "011a4966-8ec4-401e-8ce2-6b852ab21816");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7156fad9-89be-4b18-b413-aa374030e2b7");
+        }
+    }
+}
