d35d356a310f41ebc2e66124deed8575df07241d
João Otávio <joaotavioos@outlook.com>
2024-03-21 07:20:28 -0300

    feat: add sql server scripts ✨

---
+++ api_made_in_rust/src/mssql/insert.sql
@@ -0,0 +1,6 @@
+USE [FakeStore]
+
+ INSERT INTO [AspNetRoles] ([Id], [ConcurrencyStamp], [Name], [NormalizedName])
+      VALUES (N'a07d5feb-a32a-428e-9e74-6c84b78dc3ac', NULL, N'Admin', N'ADMIN'),
+             (N'de9b5614-3143-4613-b98e-a36aa8a1fa4e', NULL, N'User', N'USER')
+      
---
+++ api_made_in_rust/src/mssql/script.sql
@@ -0,0 +1,154 @@
+USE [FakeStore]
+
+CREATE TABLE [dbo].[AspNetRoleClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[RoleId] [nvarchar](450) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+ 	CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY CLUSTERED ([Id]) 
+) 
+
+CREATE TABLE [dbo].[AspNetUserLogins](
+	[LoginProvider] [nvarchar](450) NOT NULL,
+	[ProviderKey] [nvarchar](450) NOT NULL,
+	[ProviderDisplayName] [nvarchar](max) NULL,
+	[UserId] [nvarchar](450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY CLUSTERED ([LoginProvider],[ProviderKey])
+)
+
+CREATE TABLE [dbo].[__EFMigrationsHistory](
+	[MigrationId] [nvarchar](150) NOT NULL,
+	[ProductVersion] [nvarchar](32) NOT NULL,
+    CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY CLUSTERED ([MigrationId]) 
+)
+
+CREATE TABLE [dbo].[AspNetRoles](
+	[Id] [nvarchar](450) NOT NULL,
+	[Name] [nvarchar](256) NULL,
+	[NormalizedName] [nvarchar](256) NULL,
+	[ConcurrencyStamp] [nvarchar](max) NULL,
+    CONSTRAINT [PK_AspNetRoles] PRIMARY KEY CLUSTERED ([Id]) 
+)
+
+CREATE TABLE [dbo].[AspNetUserRoles](
+	[UserId] [nvarchar](450) NOT NULL,
+	[RoleId] [nvarchar](450) NOT NULL,
+ 	CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY CLUSTERED ([UserId], [RoleId])
+)
+
+CREATE TABLE [dbo].[AspNetUsers](
+	[Id] [nvarchar](450) NOT NULL,
+	[UserName] [nvarchar](256) NULL,
+	[NormalizedUserName] [nvarchar](256) NULL,
+	[Email] [nvarchar](256) NULL,
+	[NormalizedEmail] [nvarchar](256) NULL,
+	[EmailConfirmed] [bit] NOT NULL,
+	[PasswordHash] [nvarchar](max) NULL,
+	[SecurityStamp] [nvarchar](max) NULL,
+	[ConcurrencyStamp] [nvarchar](max) NULL,
+	[PhoneNumber] [nvarchar](max) NULL,
+	[PhoneNumberConfirmed] [bit] NOT NULL,
+	[TwoFactorEnabled] [bit] NOT NULL,
+	[LockoutEnd] [datetimeoffset](7) NULL,
+	[LockoutEnabled] [bit] NOT NULL,
+	[AccessFailedCount] [int] NOT NULL,
+ 	CONSTRAINT [PK_AspNetUsers] PRIMARY KEY CLUSTERED ([Id])
+) 
+
+CREATE TABLE [dbo].[AspNetUserTokens](
+	[UserId] [nvarchar](450) NOT NULL,
+	[LoginProvider] [nvarchar](450) NOT NULL,
+	[Name] [nvarchar](450) NOT NULL,
+	[Value] [nvarchar](max) NULL,
+    CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY CLUSTERED ([UserId], [LoginProvider], [Name])
+)
+
+
+CREATE TABLE [dbo].[AspNetUserClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [nvarchar](450) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+	CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY CLUSTERED ([Id])
+)
+
+CREATE TABLE [dbo].[Comments](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[StockId] [int] NULL,
+	[Title] [nvarchar](max) NOT NULL,
+	[Content] [nvarchar](max) NOT NULL,
+	[CreatedOn] [datetime2](7) NOT NULL,
+	[AppUserId] [nvarchar](450) NOT NULL,
+ 	CONSTRAINT [PK_Comments] PRIMARY KEY CLUSTERED ([Id]) 
+)
+
+CREATE TABLE [dbo].[Portfolios](
+	[AppUserId] [nvarchar](450) NOT NULL,
+	[StockId] [int] NOT NULL,
+ 	CONSTRAINT [PK_Portfolios] PRIMARY KEY CLUSTERED ([AppUserId],[StockId])
+)
+
+CREATE TABLE [dbo].[Stocks](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Symbol] [nvarchar](max) NOT NULL,
+	[CompanyName] [nvarchar](max) NOT NULL,
+	[Purchase] [decimal](18, 2) NOT NULL,
+	[LastDiv] [decimal](18, 2) NOT NULL,
+	[Industry] [nvarchar](max) NOT NULL,
+	[MarketCap] [bigint] NOT NULL,
+	CONSTRAINT [PK_Stocks] PRIMARY KEY CLUSTERED ([Id])
+  )
+
+CREATE NONCLUSTERED INDEX [IX_AspNetRoleClaims_RoleId] ON [dbo].[AspNetRoleClaims] ([RoleId])
+CREATE UNIQUE NONCLUSTERED INDEX [RoleNameIndex] ON [dbo].[AspNetRoles] ([NormalizedName])
+CREATE NONCLUSTERED INDEX [IX_AspNetUserClaims_UserId] ON [dbo].[AspNetUserClaims] ([UserId])
+CREATE NONCLUSTERED INDEX [IX_AspNetUserLogins_UserId] ON [dbo].[AspNetUserLogins] ([UserId])
+CREATE NONCLUSTERED INDEX [IX_AspNetUserRoles_RoleId] ON [dbo].[AspNetUserRoles] ([RoleId])
+CREATE NONCLUSTERED INDEX [EmailIndex] ON [dbo].[AspNetUsers] ([NormalizedEmail])
+CREATE UNIQUE NONCLUSTERED INDEX [UserNameIndex] ON [dbo].[AspNetUsers] ([NormalizedUserName]) WHERE ([NormalizedUserName] IS NOT NULL)
+CREATE NONCLUSTERED INDEX [IX_Comments_AppUserId] ON [dbo].[Comments] ([AppUserId])
+CREATE NONCLUSTERED INDEX [IX_Comments_StockId] ON [dbo].[Comments] ([StockId])
+CREATE NONCLUSTERED INDEX [IX_Portfolios_StockId] ON [dbo].[Portfolios] ([StockId])
+
+ALTER TABLE [dbo].[Comments] ADD  DEFAULT (N'') FOR [AppUserId]
+
+ALTER TABLE [dbo].[AspNetRoleClaims]  WITH CHECK ADD  CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY([RoleId]) REFERENCES [dbo].[AspNetRoles] ([Id]) ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetRoleClaims] CHECK CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId]
+
+ALTER TABLE [dbo].[AspNetUserClaims]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetUserClaims] CHECK CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId]
+
+ALTER TABLE [dbo].[AspNetUserLogins]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetUserLogins] CHECK CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId]
+
+ALTER TABLE [dbo].[AspNetUserRoles]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY([RoleId]) REFERENCES [dbo].[AspNetRoles] ([Id]) ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetUserRoles] CHECK CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId]
+
+ALTER TABLE [dbo].[AspNetUserRoles]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetUserRoles] CHECK CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId]
+
+ALTER TABLE [dbo].[AspNetUserTokens]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetUserTokens] CHECK CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId]
+
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_AspNetUsers_AppUserId] FOREIGN KEY([AppUserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_AspNetUsers_AppUserId]
+
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_Stocks_StockId] FOREIGN KEY([StockId]) REFERENCES [dbo].[Stocks] ([Id])
+
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_Stocks_StockId]
+
+ALTER TABLE [dbo].[Portfolios]  WITH CHECK ADD  CONSTRAINT [FK_Portfolios_AspNetUsers_AppUserId] FOREIGN KEY([AppUserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+
+ALTER TABLE [dbo].[Portfolios] CHECK CONSTRAINT [FK_Portfolios_AspNetUsers_AppUserId]
+
+ALTER TABLE [dbo].[Portfolios]  WITH CHECK ADD  CONSTRAINT [FK_Portfolios_Stocks_StockId] FOREIGN KEY([StockId]) REFERENCES [dbo].[Stocks] ([Id]) ON DELETE CASCADE
+
+ALTER TABLE [dbo].[Portfolios] CHECK CONSTRAINT [FK_Portfolios_Stocks_StockId]
+
